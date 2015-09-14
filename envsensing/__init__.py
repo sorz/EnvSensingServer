de4689a358d6281102ac669450d84b21aa48eebd
sorz <orz@sorz.org>
2015-09-14 18:42:21 +0800

    Blueprint structure

--- envsensing/__init__.py
+++ envsensing/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
+from .views.home import home
+
+
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object('config.default')
 app.config.from_pyfile('config.py')
 
-@app.route("/")
-def hello():
-    return "Hello, world!~"
+app.register_blueprint(home)
 
