f8d329a89892b50291cd763f4244b4ae7dbdc5a3
sorz <orz@sorz.org>
2015-12-27 03:07:58 +0800

    Add navbar links and auto active items

---
+++ envsensing/static/front.js
@@ -0,0 +1,25 @@
+
+function initializeNavbar() {
+
+  // Active item correspond to current visiting page on navbar.
+
+  var PATH_REGEX_TO_NAME = {
+    '^/users/login/': 'login',
+    '^/users/': 'users',
+    '^/$': 'home'
+  };
+
+  var path = window.location.pathname;
+  for (var pathRegex in PATH_REGEX_TO_NAME) {
+    if (path.match(pathRegex)) {
+      $('li.nav-' + PATH_REGEX_TO_NAME[pathRegex]).addClass('active');
+      break;
+    }
+  }
+}
+
+
+$(document).ready(function() {
+  initializeNavbar();
+
+});
